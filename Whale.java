5d2c610ce6424d7f47e5cd4e2558095bb491f397
Ed Travalia <33702136+etravalia@users.noreply.github.com>
2020-03-06 16:37:59 -0800

    Update Whale.java

--- Whale.java
+++ Whale.java
@@ -1,5 +1,5 @@
 // Whale Subclass
-public class Whale extends Anima l{
+public class Whale extends Animal {
 	public Whale (String n, String f, String s) {
 		super(n, "krill", "eeOORMorrgghhh");
 	}
