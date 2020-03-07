f3642811eec878b9bef61a453fadc8b4cff47f47
Ed Travalia <33702136+etravalia@users.noreply.github.com>
2020-03-06 16:29:52 -0800

    Create Zoo.java

---
+++ Zoo.java
@@ -0,0 +1,30 @@
+Zoo Super Class
+
+public class Zoo {
+    private Exhibits[] zooExhibits;
+    private int num;
+    private int max;
+    
+    public void Zoo(int max) {
+   	for (int i = 0; i < max; i++) {
+   		 zooExhibits[i] = null;
+   	 }
+   	 this.max = max;
+    }
+    
+    public void addExhibit(Exhibit e) {
+   	 if (max > num) {
+   	 zooExhibits[num] = e;
+   	 num++;
+   	 }
+   	 else {
+   		 throw new IllegalArgumentException("Max Exhibits Reached");
+   	 }
+    }
+    
+    public Exhibits[] getExhibits() {
+   	 return zooExhibits;
+    }
+}
+
+
