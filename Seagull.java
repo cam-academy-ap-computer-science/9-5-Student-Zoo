bab96e5858f0275564df21a3d461173d44d48201
Ed Travalia <33702136+etravalia@users.noreply.github.com>
2020-03-06 16:33:41 -0800

    Create Seagull.java

---
+++ Seagull.java
@@ -0,0 +1,7 @@
+// Seagull(“new science” definition of mammal) Subclass
+public class Seagull extends Animal {
+	public Seagull (String n, String f, String s) {
+		super(n, "crab", "mine!mine!mine!");
+	}
+}
+
