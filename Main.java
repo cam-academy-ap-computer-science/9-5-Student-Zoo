a1fe924ece47a4979630e1c815ba89f668f90ba7
Ed Travalia <33702136+etravalia@users.noreply.github.com>
2020-03-06 16:28:41 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+Main	
+public static void Zoo() {
+		populateMyZoo();
+	}
+	public static void populateMyZoo() {
+		int count = 0;
+		Exibit[] exibits = new Exibit[10];
+		Animal[] animals = new animals[10];
+		for (int i = 0; i <= exibits[].length(); i++) {
+			for(int j = 0; j <= animals[].length; j++) {
+				count++;
+				System.out.println(toString());
+			}
+		}
+		System.out.print(count);
+	}
+	
+
+}
